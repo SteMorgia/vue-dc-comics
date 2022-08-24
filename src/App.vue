e4ed49d3d8fb15c0f8080b1448958d3fb77de94b
Stefano Morgia <ste.morgia@gmail.com>
2022-08-24 15:07:47 +0200

    header developing in progress

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-    
+    <MyHeader/>
   </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader'
 
 
 export default {
   name: 'App',
   components: {
+    MyHeader,
     
-  }
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './styles/general.scss';
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,78 @@
+<template>
+    <header>
+        <img src="../assets/img/dc-logo.png" alt="dc-logo">
+        <nav>
+            <ul>
+                <li v-for="(link, index) in links" :key="index" >
+                    <a :href="link.url" :class="link.active?'active':''" >{{link.text}}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+    data() {
+        return {
+            links: [
+                {
+                    text: 'CHARACTERS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'COMICS',
+                    url: '#',
+                    active: true
+                },
+                {
+                    text: 'MOVIES',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'GAMES',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'VIDEOS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'FANS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'NEWS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'SHOP',
+                    url: '#',
+                    active: false
+                }
+            ]
+        }
+    }
+    }
+</script>
+
+<style>
+
+</style>
